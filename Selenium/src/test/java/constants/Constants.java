0b45ec49daf3de9beecf436c5d07648e189be966
lukeharrison95 <58259358+lukeharrison95@users.noreply.github.com>
2020-02-06 11:19:11 +0000

    Update Constants.java

--- Selenium/src/test/java/constants/Constants.java
+++ Selenium/src/test/java/constants/Constants.java
@@ -6,7 +6,7 @@ package constants;
 
 		public static final String PATH = "chromedriver";
 		public static final String PROPERTY = "webdriver.chrome.driver";
-		public static final String ip ="3.10.118.80:80";
+		public static final String ip ="http://localhost:80";
 		
 		private Constants() {
 
